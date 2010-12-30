89bbe4f4bc8f8fe05a64b73b9f97508a2efaa1a9
fpstovall@gmail.com <>
2010-12-29 22:05:40 -0500

    Corrected a stupid but pervasive spelling error in the transceiver class.

--- common/transceiver/transceiver.h
+++ common/transceiver/transceiver.h
@@ -16,8 +16,8 @@
  
  **********************************************/
  
-#ifndef nrtb_transciever_h
-#define nrtb_transciever_h
+#ifndef nrtb_transceiver_h
+#define nrtb_transceiver_h
 
 #include <string>
 #include <Poco/Net/StreamSocket.h>
@@ -29,7 +29,7 @@
 namespace nrtb
 {
   /*******************************************************************
-   * transciever is used to manage GBP (google protocol buffers)
+   * transceiver is used to manage GBP (google protocol buffers)
    * traffic between NRTB components. This is the only form of 
    * IPC used by NRTB.
    * 
@@ -39,16 +39,16 @@ namespace nrtb
    * The nrtb::confreader singleton will be queried for the 
    * following parameters:
    * 
-   * 	transciever.history_size			(int)
-   * 	transciever.send_timeout			(int)
-   * 	transciever.allow_recovery			(bool)
-   * 	transciever.max_consecutive_errors	(int)
+   * 	transceiver.history_size			(int)
+   * 	transceiver.send_timeout			(int)
+   * 	transceiver.allow_recovery			(bool)
+   * 	transceiver.max_consecutive_errors	(int)
    * 
    * See https://blueprints.launchpad.net/nrtb/+spec/icp-spec for
    * specification this class implements.
    * ***************************************************************/
   template <class out_gpb, class in_gpb>
-  class transciever
+  class transceiver
   {
 	public:
 	  /// outbound messages will be of this type
@@ -56,16 +56,16 @@ namespace nrtb
 	  /// inbound messages will be of this type.
 	  typedef in_gpb inbound_type;
 	  /**************************************************************
-	   * Creates the transciever and associates it with a provided 
+	   * Creates the transceiver and associates it with a provided 
 	   * socket. Once created this class assumes it uniquely owns the 
 	   * socket and will close it upon distruction.
 	   * ***********************************************************/
-	  transciever(Poco::Net::StreamSocket socket);
+	  transceiver(Poco::Net::StreamSocket socket);
 	  /**************************************************************
 	   * Closes the socket and releases all mmemory associated with
 	   * this class.
 	   * ***********************************************************/
-	  ~transciever();
+	  ~transceiver();
 	  /**************************************************************
 	   * gets the next message from the socket. If no messages are 
 	   * ready, blocks util one arrives. 
@@ -91,18 +91,18 @@ namespace nrtb
 	  /**************************************************************
 	   * Exceptions which may be thrown for external resulution.
 	   * ***********************************************************/
-	  // parent of all transciever exceptions
-	  POCO_DECLARE_EXCEPTION(transciever, general_exception, Poco::Exception)
+	  // parent of all transceiver exceptions
+	  POCO_DECLARE_EXCEPTION(transceiver, general_exception, Poco::Exception)
 	  // thrown if send fails due to timeout.
-	  POCO_DECLARE_EXCEPTION(transciever, send_timeout, general_exception)
+	  POCO_DECLARE_EXCEPTION(transceiver, send_timeout, general_exception)
 	  // thrown in the case of a fault while in get()
-	  POCO_DECLARE_EXCEPTION(transciever, get_fault, general_exception)
+	  POCO_DECLARE_EXCEPTION(transceiver, get_fault, general_exception)
 	  // Thrown if the socket is closed due to an unrecoverable date error.
-	  POCO_DECLARE_EXCEPTION(transciever, unrecoverable_data_error, general_exception)
+	  POCO_DECLARE_EXCEPTION(transceiver, unrecoverable_data_error, general_exception)
 	  // Thrown if the socket is closed due to too many errors in a row
-	  POCO_DECLARE_EXCEPTION(transciever, consecutive_error_overrun, general_exception)
+	  POCO_DECLARE_EXCEPTION(transceiver, consecutive_error_overrun, general_exception)
 	protected:
-	  const std::string logname = "transciever:";
+	  const std::string logname = "transceiver:";
 	  unsigned int send_time_limit;
 	  bool attempt_recovery;
 	  unsigned int error_run_limit;
@@ -124,4 +124,4 @@ namespace nrtb
 
 } // namespace nrtb
  
-#endif //nrtb_transciever_h
+#endif //nrtb_transceiver_h
