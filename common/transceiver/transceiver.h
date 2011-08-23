135fb990c1308dcbe77ff6b1dc7174b84a942196
fpstovall@gmail.com <>
2011-08-23 00:25:38 -0400

    Working versions of the transciver and its unit test program. There is still one outstanding error occuring when the sending message number is 0, it seems that number does not encode properly (it is recieved as 13 instead of 0), making it impossible to sync up the sequence number check.

--- common/transceiver/transceiver.h
+++ common/transceiver/transceiver.h
@@ -20,8 +20,10 @@
 #define nrtb_transceiver_h
 
 #include <string>
+#include <sstream>
 #include <base_socket.h>
 #include <serializer.h>
+#include <confreader.h>
 #include <Poco/Logger.h>
 #include <boost/shared_ptr.hpp>
 #include <boost/circular_buffer.hpp>
@@ -47,23 +49,14 @@ namespace nrtb
    * See https://blueprints.launchpad.net/nrtb/+spec/icp-spec for
    * specification this class implements.
    * ***************************************************************/
-  
-  /****************************************
-   * This class is used ot manage all interprocess
-   * communications between programs in the NRTB
-   * system.
-   * 
-   * inp and outp are expected to be types 
-   * of the form boost::shared_ptr<GPB> message,
-   * normally the channel wrappers for the 
-   * link to be managed. 
-   ***************************************/
   template <class out, class in,
 	class outp = boost::shared_ptr<out>,
 	class inp = boost::shared_ptr<in> >
   class transceiver
   {
 	public:
+	  typedef inp in_ptr;
+	  typedef outp out_ptr;
 	  
 	  /*************************************************************
 	   * Creates the transceiver and associates it with a provided 
@@ -86,7 +79,7 @@ namespace nrtb
 	   * sent_messages buffer in case it's needed for error recovery.
 	   * ***********************************************************/
 	  void send(outp sendme);
-	  /**************************************************************
+	  /**q************************************************************
 	   * Called by the data consumer when an inbound message was 
 	   * not valid in the current application context. msg_number
 	   * is the sequence number of the offending message.
@@ -114,8 +107,8 @@ namespace nrtb
 	  unsigned int send_time_limit;
 	  bool attempt_recovery;
 	  unsigned int error_run_limit;
-	  /// pointer to this class's logger instance
-	  Poco::Logger * log;
+	  /// the name used for logging
+	  std::string logname;
 	  /// The socket used for communcation.
 	  tcp_socketp sock;
 	  /// serializer used for message numbers
@@ -131,6 +124,122 @@ namespace nrtb
 	  void handle_inbound_nak();
 	  void handle_outbound_nak();
   };
+  
+
+serializer tscvr_sequence(0);
+
+template <class out, class in, class outp, class inp>
+transceiver<out,in,outp,inp>::transceiver(tcp_socketp socket)
+{
+  // get the configuration parameters.
+  global_conf_reader & config = global_conf_reader::get_instance();
+  send_time_limit = config.get<unsigned int>("transceiver.send_timeout",2);
+  attempt_recovery = config.get<bool>("transceiver.allow_recovery",true);
+  error_run_limit = 
+	config.get<unsigned int>("transceiver.max_consecutive_errors",10);
+  sent_messages.resize(config.get<int>("transceiver.history_size",50));
+  last_inbound = 0;
+  // set up logging
+  std::stringstream s;
+  s << "transceiver:_" << tscvr_sequence();
+  logname = s.str();
+  Poco::Logger & log = Poco::Logger::get(logname);
+  // set up the socket.
+  sock = socket;
+  // annouce ourselves...
+  log.trace("Instanciated."); 
+  s.str("");
+  s << "history_size=" << sent_messages.size()
+    << ", send_timeout=" << send_time_limit
+    << ", attempt_recovery=" << attempt_recovery
+    << ", error_run_limit=" << error_run_limit
+    << ", remote_address=" << sock->get_remote_address()
+    << ", local_address=" << sock->get_local_address();
+  log.trace(s.str());
+};
+
+template <class out, class in, class outp, class inp>
+transceiver<out,in,outp,inp>::~transceiver()
+{
+	Poco::Logger & log = Poco::Logger::get(logname);
+	log.trace("In ~transciever");
+	// shutdown the socket.
+	sock->close();
+	// discard the sent messages list.
+	sent_messages.clear();
+	log.trace("shutdown complete.");
+};
+
+template <class out, class in, class outp, class inp>
+inp transceiver<out,in,outp,inp>::get()
+{
+  inp returnme(new in);
+  std::string input = sock->getln();
+  returnme->ParseFromString(input);
+  // for the first messsge any number is
+  // accepted.
+  if (last_inbound == 0)
+  {
+	last_inbound = returnme->msg_uid();
+  }
+  else
+  {
+	last_inbound++;
+	int temp = returnme->msg_uid();
+	if (temp != last_inbound)
+	{ 
+	  inbound_seq_error e;
+	  std::stringstream message;
+	  message << "Expected " << last_inbound
+		<< " received "  << temp;
+	  e.store(message.str());
+	  throw e;
+	};
+  };
+  return returnme;
+};
+
+template <class out, class in, class outp, class inp>
+void transceiver<out,in,outp,inp>::send(outp sendme)
+{
+  sendme->set_msg_uid(out_msg_num());
+  std::string output;
+  output = sendme->SerializeAsString() + "\r";
+  sock->put(output);
+  sent_messages.push_back(sendme);
+};
+
+template <class out, class in, class outp, class inp>
+void transceiver<out,in,outp,inp>::nak_invalid_context(const unsigned long int msg_number)
+{
+  general_exception e;
+  e.store("transceiver<outp,inp>::nak_invalid_context not implemented.");
+  throw e;
+};
+
+template <class out, class in, class outp, class inp>
+void transceiver<out,in,outp,inp>::nak_validation_error(const unsigned long int msg_number)
+{
+  general_exception e;
+  e.store("transceiver<outp,inp>::nak_validation_error not implemented.");
+  throw e;
+};
+
+template <class out, class in, class outp, class inp>
+void transceiver<out,in,outp,inp>::handle_inbound_nak()
+{
+  general_exception e;
+  e.store("transceiver<outp,inp>::handle_inbound_nak not implemented.");
+  throw e;  
+};
+
+template <class out, class in, class outp, class inp>
+void transceiver<out,in,outp,inp>::handle_outbound_nak()
+{
+  general_exception e;
+  e.store("transceiver<outp,inp>::handle_outbound_nak not implemented.");
+  throw e;    
+};
 
 } // namespace nrtb
  
--- common/transceiver/transceiver_test.cpp
+++ common/transceiver/transceiver_test.cpp
@@ -16,18 +16,130 @@
  
  **********************************************/
  
+#include <string>
+#include <iostream>
+#include <base_thread.h>
+#include <base_socket.h>
 #include "transceiver.h"
 #include <log_setup.h>
 #include <sim_to_db_wrapper.pb.h>
+#include <boost/random.hpp>
 
 using namespace nrtb;
+using namespace std;
+
+typedef nrtb_msg::sim_to_db my_msg;
+typedef boost::shared_ptr<my_msg> my_msgp;
+typedef transceiver<my_msg,my_msg> linkt;
+
+int er_count = 0;
+
+class server_work_thread: public runnable
+{
+public:
+  
+  tcp_socketp sock;
+  unsigned long long last_inbound;
+  
+  void run()
+  {
+	linkt link(sock);
+	while (sock->status() == tcp_socket::sock_connect)
+	{
+	  try 
+	  {
+		cout << "'\tserver waiting" << endl;
+		linkt::out_ptr inbound = link.get();
+		cout << "\tserver got message" << endl;
+		last_inbound = inbound->msg_uid();
+		cout << "\tReceived #" << last_inbound << endl;
+	  }
+	  catch (linkt::general_exception & e)
+	  {
+		cerr << "Server work thread caught " << e.what()
+		  << "\n\tComment: " << e.comment() << endl;
+		er_count++;
+	  }
+	  catch (tcp_socket::general_exception & e)
+	  {
+		cerr << "Server work thread caught " << e.what()
+		  << "\n\tComment: " << e.comment() << endl;
+		er_count++;
+	  }
+	  catch (std::exception & e)
+	  {
+		cerr << "Server work thread caught " << e.what() 
+		  << endl;
+		er_count++;
+	  };
+	};
+  };
+};
+
+thread process;
+server_work_thread task;
+  
+class listener: public tcp_server_socket_factory
+{
+public:
+  listener(const string & add, const int & back)
+   : tcp_server_socket_factory(add, back) {};
+  
+  void on_accept()
+  {
+	if (!process.is_running())
+	{
+	  task.last_inbound = 0;
+	  task.sock.reset(connect_sock);
+	  process.start(task);
+	  cout << "server thread running." << endl;
+	}
+	else
+	{
+	  tcp_socketp s(connect_sock);
+	  s->close();
+	  cerr << "Multiple attempts to connect to server" 
+		<< endl;
+	};
+  };
+};
+
+string address = "127.0.0.1:";
+int port_base = 12334;
 
 int main()
 {
-  int er_count = 0;
   setup_global_logging("transceiver.log");
   
-  
-  
+  //set up our port and address
+  boost::mt19937 rng;
+  rng.seed(time(0));
+  boost::uniform_int<> r(0,1000);
+  stringstream s;
+  s << address << port_base + r(rng);
+  address = s.str();
+  cout << "Using " << address << endl;
+
+  // kick off the listener thread.
+  listener server(address,5);
+  server.start_listen();
+  usleep(5e5);
+
+  // set up our sender
+  tcp_socketp sock(new tcp_socket);
+  sock->connect(address);
+  linkt sender(sock);
+
+  // Let's send a few things.
+  for (int i=0; i<100; i++)
+  {
+	my_msgp msg(new my_msg);
+	sender.send(msg);
+	cout << "Sent " << msg->msg_uid() << endl;
+	usleep(1e4);
+  };
+    
+  //er_count = 113 - task.last_inbound;
+  //cout << task.last_inbound;
   return er_count;
 };
