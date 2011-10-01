25eabca07246d41e66dc2147412e11b519ebb9df
fpstovall@gmail.com <>
2011-10-01 19:14:01 -0400

    Completed coding on the circular_queue class.. need to do the unit next.

--- common/circular_queue/circular_queue.h
+++ common/circular_queue/circular_queue.h
@@ -25,58 +25,112 @@
 namespace nrtb
 {
 
-  /********************************************************
-   * The circular_queue template is designed for use with 
-   * the classic producer/consumer thread management model. 
-   * The producer uses circular_queue::push() to put items
-   * in the queue as they become available, and the consumer
-   * thread calls circular_queue::park() when it is ready 
-   * for the next item to work.
-   * 
-   * Common uses would be for buffering outgoing or incomming
-   * messages from a communications channel, providing a feed
-   * queue for parallel threads to make full use of multi-core 
-   * processors, or any case where one or more threads are 
-   * passing data to another set of threads.
-  ********************************************************/
-  template <class T>
-  class cirular_queue
-  {
-  public:
-	
-	/*********************************************
-	 * creates the queue with the specified 
-	 * number of elements. All memory is allocated
-	 * at construction to minimize delays at runtime.
-	*********************************************/
-	circular_queue(int size);
-	
-	/*********************************************
-	 * releases all items in the queue
-	*********************************************/
-	virtual ~circular_queue();
-	
-	/*********************************************
-	 * Puts an item in the queue. 
-	*********************************************/
-	void push(T);
-		
-	/*********************************************
-	 * Pops the next item off the queue, blocking 
-	 * if needed until an item becomes available. 
-	*********************************************/
-	T pop();
-	
-	int size();
-	void resize(int newsize);
-	void clear();
-	
-  protected:
-	
-	boost::circular_buffer<T>
-	cond_variable buffer_lock;
-  };
+/********************************************************
+ * The circular_queue template is designed for use with 
+ * the classic producer/consumer thread management model. 
+ * The producer uses circular_queue::push() to put items
+ * in the queue as they become available, and the consumer
+ * thread calls circular_queue::park() when it is ready 
+ * for the next item to work.
+ * 
+ * Common uses would be for buffering outgoing or incomming
+ * messages from a communications channel, providing a feed
+ * queue for parallel threads to make full use of multi-core 
+ * processors, or any case where one or more threads are 
+ * passing data to another set of threads.
+********************************************************/
+template <class T>
+class cirular_queue
+{
+public:
   
+  /*********************************************
+	* creates the queue with the specified 
+	* number of elements. All memory is allocated
+	* at construction to minimize delays at runtime.
+  *********************************************/
+  circular_queue(int size);
+  
+  /*********************************************
+	* releases all items in the queue
+  *********************************************/
+  virtual ~circular_queue();
+  
+  /*********************************************
+	* Puts an item in the queue. 
+  *********************************************/
+  void push(T item);
+	  
+  /*********************************************
+	* Pops the next item off the queue, blocking 
+	* if needed until an item becomes available. 
+  *********************************************/
+  T pop();
+  
+  int size();
+  void resize(int newsize);
+  void clear();
+  
+protected:
+  
+  boost::circular_buffer<T> buffer;
+  cond_variable buffer_lock;
+};
+  
+template <class T>
+cirular_queue::circular_queue<T>(int size)
+{
+  buffer.resize(int);
+};
+
+template <class T>
+cirular_queue<T>::~circular_queue()
+{
+  scope_lock lock(buffer_lock);
+  buffer.clear();
+};
+
+template <class T>
+void cirular_queue<T>::push(T item)
+{
+  scope_lock lock(buffer_lock);
+  buffer.push_back(item);
+  buffer_lock.signal();
+};
+
+template <class T>
+T cirular_queue<T>::pop()
+{
+  scope_lock lock(buffer_lock);
+  while (buffer.empty())
+	buffer_lock.wait();
+  T returnme = buffer.front();
+  buffer.pop_front();
+  return returnme;
+};
+
+template <class T>
+int cirular_queue<T>::size()
+{
+  scope_lock lock(buffer_lock);
+  return buffer_lock.size();
+};
+
+template <class T>
+void cirular_queue<T>::resize(int newsize)
+{
+  scope_lock lock(buffer_lock);
+  buffer.resize(newsize);
+};
+
+template <class T>
+void cirular_queue<T>::clear()
+{
+  scope_lock lock(buffer_lock);
+  buffer.clear();
+};
+
+
 } // namespace nrtb
  
 #endif //nrtb_circular_queue_h//
