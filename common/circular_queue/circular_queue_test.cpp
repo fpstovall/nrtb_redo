71c93d836fb70d6e6b74b62e5b39c2e9376d838e
fpstovall@gmail.com <>
2011-10-06 16:09:07 -0400

    Completed the nrtb::circular_queue, a high speed queue for interthread communication. This is ready to merge to the main alpha branch.

--- common/circular_queue/circular_queue_test.cpp
+++ common/circular_queue/circular_queue_test.cpp
@@ -18,188 +18,135 @@
  
 #include <string>
 #include <iostream>
-#include <base_thread.h>
-#include <base_socket.h>
-#include "transceiver.h"
-#include <log_setup.h>
-#include <sim_to_db_wrapper.pb.h>
-#include <boost/random.hpp>
+#include "circular_queue.h"
+#include <boost/shared_ptr.hpp>
 
 using namespace nrtb;
 using namespace std;
 
-typedef nrtb_msg::sim_to_db my_msg;
-typedef transceiver<my_msg,my_msg> linkt;
+typedef circular_queue<int> test_queue;
+typedef boost::shared_ptr<test_queue> queue_p;
 
-class safe_counter
-{
-private:
-  mutex data_lock;
-  int er_count;
-
-public:
-  
-  safe_counter() { er_count = 0; };
-
-  ~safe_counter() {};
-
-  void inc()
-  {
-	scope_lock lock(data_lock);
-	er_count++;
-  };
-
-  int operator ()()
-  {
-	scope_lock lock(data_lock);
-	return er_count;
-  };
-};
-
-safe_counter er_count;
-
-class server_work_thread: public thread
+class consumer_task: public thread
 {
 public:
-  
-  tcp_socket_p sock;
-  unsigned long long last_inbound;
-  
-  ~server_work_thread()
+
+  consumer_task(string n, queue_p buffer)
   {
-	cout << "Destructing server_work_thread" << endl;
-	sock.reset();
+	name = n;
+	input = buffer;
+	count = 0;
   };
+  
+  ~consumer_task()
+  {
+	cout << ">> in " << name << "::~consumer_task()" << endl;
+	try
+	{
+	  this->thread::~thread();
+	  input.reset();
+	}
+	catch (...)  {};
+	cout << "<< leaving " << name << "::~consumer_task()" << endl;
+  };
+  
+  int get_count() { return count; };
   
   void run()
   {
-	set_cancel_anytime();
-	linkt link(sock);
-	while (sock->status() == tcp_socket::sock_connect)
+	try
 	{
-	  try 
+	  while (true)
 	  {
-		linkt::out_ptr inbound = link.get();
-		last_inbound = inbound->msg_uid();
-		cout << "\tReceived #" << last_inbound << endl;
-		link.send(inbound);
-		if (last_inbound == 99)
+		int num = input->pop();
 		{
-		  cout << "Receiver thread closing." << endl;
-		  exit(0);
+		  static mutex console;
+		  scope_lock lock(console);
+		  cout << name << " picked up " << num
+			<< endl;
 		};
+		count++;
+		lastnum = num;
+		yield();	  
 	  }
-	  catch (linkt::general_exception & e)
-	  {
-		cerr << "Server work thread caught " << e.what()
-		  << "\n\tComment: " << e.comment() << endl;
-		er_count.inc();;
-	  }
-	  catch (tcp_socket::general_exception & e)
-	  {
-		cerr << "Server work thread caught " << e.what()
-		  << "\n\tComment: " << e.comment() << endl;
-		er_count.inc();;
-	  }
-	  catch (std::exception & e)
-	  {
-		cerr << "Server work thread caught " << e.what() 
-		  << endl;
-		er_count.inc();;
-	  };
-	};
-  };
-};
-
-class listener: public tcp_server_socket_factory
-{
-protected:
-  boost::shared_ptr<server_work_thread> task;
-
-public:
-  listener(const string & add, const int & back)
-   : tcp_server_socket_factory(add, back) {};
-  ~listener()
-  {
-	cout << "Destructing listener" << endl;
-	task.reset();
-  };
-  
-  bool on_accept()
-  {
-	if (!task)
-	{
-	  task.reset(new server_work_thread);
-	  task->last_inbound = 0;
-	  task->sock = connect_sock;
-	  task->start(*(task.get()));
-	  cout << "server thread running." << endl;
-	  // shutdown the listener thead.. our work is done here.
-	  return false;
 	}
-	else
-	{
-	  connect_sock->close();
-	  cerr << "Multiple attempts to connect to server" 
-		<< endl;
-	};
+	catch (...) {};
   };
+
+protected:
+  // link to the feed queue
+  queue_p input;
+  // a name to report 
+  string name;
+  // number of items processed
+  int count;
+  // last number caught
+  int lastnum;
 };
 
-string address = "127.0.0.1:";
-int port_base = 12334;
+typedef boost::shared_ptr<consumer_task> task_p;
+
 
 int main()
 {
-  setup_global_logging("transceiver.log");
-
-  try
+  int er_count = 0;
+  /************************************************
+   * Load queue and then cook it down...
+   ***********************************************/
+  // make and load a queue
+  queue_p q1(new test_queue(50));
+  for (int i=0; i<100; i++)
   {
-	//set up our port and address
-	boost::mt19937 rng;
-	rng.seed(time(0));
-	boost::uniform_int<> r(0,1000);
-	stringstream s;
-	s << address << port_base + r(rng);
-	address = s.str();
-	cout << "Using " << address << endl;
-
-	// kick off the listener thread.
-	listener server(address,5);
-	server.start_listen();
-	usleep(1e4);
-
-	// set up our sender
-	tcp_socket_p sock(new tcp_socket);
-	sock->connect(address);
-	linkt sender(sock);
-
-	// Let's send a few things.
-	for (int i=0; i<100; i++)
-	{
-	  linkt::out_ptr msg(new my_msg);
-	  sender.send(msg);
-	  cout << "Sent " << msg->msg_uid() << endl;
-	  msg = sender.get();
-	  cout << "Got back " << msg->msg_uid() << endl;
-	};
-	usleep(1e4);
-  }
-  catch (...)
-  {
-	cout << "exception caught during test." << endl;
-	er_count.inc();
+	q1->push(i);
   };
-
-  int faults = er_count(); 
-  if (faults)
+  // the queue should be loaded with 50-99
+  // attach a thread and process it.
+  task_p p1(new consumer_task("task 1",q1));
+  p1->start();
+  while (q1->size()) usleep(100);
+  cout << "cp 1 " << p1->get_count() << endl;
+  /************************************************
+   * now that the preload is exhasted, shove items
+   * in one at a time to make sure each is picked
+   * up correctly.
+   ***********************************************/
+  for (int i=200; i<225; i++)
   {
-	cout << "========== ** There were " << faults 
-	  << "errors logged. =========" << endl; 
-  }
-  else
-	cout << "========= nrtb::transceiver test complete.=========" 
-	  << endl;
-
-  return faults;
+	q1->push(i);
+	usleep(100);
+  };
+  cout << "cp 2 " << p1->get_count() << endl;
+  /************************************************
+   * Last check; attach a second thread to the queue
+   * and make sure both are servicing it.
+   ***********************************************/
+  task_p p2(new consumer_task("task 2",q1));
+  p2->start();
+  for (int i=300; i<325; i++)
+  {
+	q1->push(i);
+  };
+  while (q1->size()) usleep(100);
+  // shut it all down
+  q1->shutdown();
+  p1->join();
+  p2->join();
+  // important numbers
+  int tot_items = p1->get_count() + p2->get_count();
+  int p1_items = p1->get_count() - 75;
+  int p2_items = p2->get_count();
+  // release she threads and queues.
+  p1.reset();
+  p2.reset();
+  q1.reset();
+  // do some reporting.
+  cout << "cp 3 " 
+	<<  tot_items
+	<< " [75 + (" << p1_items
+	<< " + " << p2_items 
+	<< ")]" << endl;
+	bool passed = (tot_items == 100);
+  // inverted logic needed because 0 is good for 
+  // return codes.
+  return !passed; 
 };
