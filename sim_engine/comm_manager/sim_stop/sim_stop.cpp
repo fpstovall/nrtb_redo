66cc1bc6f9ac7a0d4afe5f7cf2c6a3d172a1fd40
fpstovall@gmail.com <>
2011-12-06 05:43:16 -0500

    Completed nrtb_com::request_sim_stop; a handler for the db_request_sim_stop GPB message and the last of the channel/simulation management messsages needed.

---
+++ sim_engine/comm_manager/sim_stop/sim_stop.cpp
@@ -0,0 +1,63 @@
+/***********************************************
+ This file is part of the NRTB project (https://*launchpad.net/nrtb).
+ 
+ NRTB is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NRTB is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NRTB.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+
+#include "sim_stop.h"
+
+#include <iostream>
+
+namespace nrtb_com
+{
+
+com_request_sim_stop::com_request_sim_stop()
+{
+  failed = false;
+  reason = "";
+};
+
+com_request_sim_stop::com_request_sim_stop(nrtb_msg::db_request_sim_stop* ext)
+{
+  set_from_message(ext);
+};
+
+void com_request_sim_stop::set_from_message(nrtb_msg::db_request_sim_stop* ext)
+{
+  failed = ext->failed();
+  if (ext->has_reason())
+	reason = ext->reason();
+  else
+	reason = "";
+};
+
+void com_request_sim_stop::load_message(nrtb_msg::db_request_sim_stop* ext)
+{
+  ext->set_failed(failed);
+  if (reason != "")
+	ext->set_reason(reason);
+};
+
+bool com_request_sim_stop::operator==(com_request_sim_stop& a)
+{
+  bool returnme = true;
+  returnme = (failed == a.failed) and returnme;
+  returnme = (reason == a.reason) and returnme;
+  return returnme;
+};
+
+  
+} // namespace nrtb::com
+
