8cbc2d79f517529bb2c0723f76b4c339ebd101f8
Rick Stovall <fpstovall>
2013-02-05 11:59:28 -0500

    Start of the thread pool redesign... including a global pool registry.

--- D_lang/testing/thread_pool/thread_pool.mm
+++ D_lang/testing/thread_pool/thread_pool.mm
@@ -1,39 +1,31 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1199190455281" ID="ID_1574918623" MODIFIED="1199190480522" TEXT="thread_pool">
-<node CREATED="1357355872013" ID="ID_1802947813" MODIFIED="1357357270302" POSITION="right" TEXT="@property uint thread_count"/>
-<node CREATED="1357356029266" ID="ID_138993489" MODIFIED="1357356345466" POSITION="right" TEXT="@property uint free_threads"/>
-<node CREATED="1357358439387" ID="ID_1200218808" MODIFIED="1357358446392" POSITION="right" TEXT="@property low_water"/>
-<node CREATED="1357358447507" ID="ID_1780124154" MODIFIED="1357358455528" POSITION="right" TEXT="@property high_water"/>
-<node CREATED="1357358456371" ID="ID_1168370966" MODIFIED="1357358485632" POSITION="right" TEXT="@property increment"/>
-<node CREATED="1357358491818" ID="ID_1188843464" MODIFIED="1357358571535" POSITION="right" TEXT="void set_levels(in uint low, in uint high, in uint increment)"/>
-<node CREATED="1357358393243" ID="ID_742632506" MODIFIED="1357358401512" POSITION="right" TEXT="tid listener_tid"/>
-<node CREATED="1357356274196" ID="ID_1073211966" MODIFIED="1357357779373" POSITION="right" TEXT="(T) // Data packet type"/>
-<node CREATED="1357357780374" ID="ID_1278207219" MODIFIED="1357358422011" POSITION="right" TEXT="stuct {T data; tid requestor; bool done} work_packet"/>
-<node CREATED="1357357090890" ID="ID_1871103626" MODIFIED="1357358436384" POSITION="right" TEXT="tid[] available_threads"/>
-<node CREATED="1357356540428" ID="ID_948147143" MODIFIED="1357356844977" POSITION="right" TEXT="void enqueue(T) (in T data, in tid returnto)">
-<node CREATED="1357358729705" ID="ID_71779585" MODIFIED="1357358773182" TEXT="if free_threads &lt; low_water start (increment) new threads"/>
-<node CREATED="1357358260668" ID="ID_1129836507" MODIFIED="1357358320051" TEXT="assemble work_packet"/>
-<node CREATED="1357358334211" ID="ID_116198003" MODIFIED="1357358343689" TEXT="set workpacket.done false"/>
-<node CREATED="1357358344891" ID="ID_979460417" MODIFIED="1357412502419" TEXT="send work_packet to first available worker"/>
-<node CREATED="1357412504102" ID="ID_1155499415" MODIFIED="1357412516125" TEXT="remove worker from available queue"/>
+<node CREATED="1360081563697" ID="ID_729586653" MODIFIED="1360082647769" POSITION="right" TEXT="global pool manager()">
+<node CREATED="1360082650811" ID="ID_1732889072" MODIFIED="1360082831445" TEXT="Free running thread"/>
+<node CREATED="1360082832267" ID="ID_630625220" MODIFIED="1360082838829" TEXT="On startup">
+<node CREATED="1360082840526" ID="ID_715527423" MODIFIED="1360082857649" TEXT="places thisTid in an accessable location"/>
+<node CREATED="1360082860198" ID="ID_1561203760" MODIFIED="1360082884250" TEXT="prepares a clear task pool list"/>
 </node>
-<node CREATED="1357356882411" ID="ID_1556184124" MODIFIED="1357356933049" POSITION="right" TEXT="void listener()">
-<node CREATED="1357357018682" ID="ID_1539153576" MODIFIED="1357419298508" TEXT="on recept of (T)">
-<node CREATED="1357413007865" ID="ID_1301738333" MODIFIED="1357413061232" TEXT="call enqueue(T, sender_tid)"/>
+<node CREATED="1360082900551" ID="ID_317519965" MODIFIED="1360082911045" TEXT="service loop">
+<node CREATED="1360082912723" ID="ID_1377548506" MODIFIED="1360082954302" TEXT="rcv Tid, string">
+<node CREATED="1360083028525" ID="ID_1807781309" MODIFIED="1360083218933" TEXT="return task pool Tid if exists"/>
+<node CREATED="1360083086752" ID="ID_1950163396" MODIFIED="1360083109710" TEXT="if string not registered, return excepiton"/>
 </node>
-<node CREATED="1357357290001" ID="ID_662876327" MODIFIED="1357357438590" TEXT="on recept of this.close, shutdown all threads"/>
-<node CREATED="1357357498008" ID="ID_819573878" MODIFIED="1357357542590" TEXT="on recept of this.available, list sender in available threads"/>
-<node CREATED="1357357929342" ID="ID_630466535" MODIFIED="1357357958003" TEXT="on recept of work_packet">
-<node CREATED="1357357958573" ID="ID_67627102" MODIFIED="1357357973435" TEXT="return data to requestor"/>
-<node CREATED="1357357974501" ID="ID_265677790" MODIFIED="1357357986274" TEXT="list sender in available threads"/>
+<node CREATED="1360082955756" ID="ID_744173745" MODIFIED="1360083256922" TEXT="rcv Tid, Tid id, bool">
+<node CREATED="1360083260726" ID="ID_1876022998" MODIFIED="1360083296103" TEXT="if id exists and bool is false, deregister id"/>
+<node CREATED="1360083310005" ID="ID_1649250749" MODIFIED="1360083323289" TEXT="if id does not exist, return exception"/>
+</node>
+<node CREATED="1360083007935" ID="ID_966938546" MODIFIED="1360083025868" TEXT="rcv Tid, Tid, string">
+<node CREATED="1360083342249" ID="ID_730280620" MODIFIED="1360083450324" TEXT="if name|Tid not in use, register name/Tid pairing"/>
+<node CREATED="1360083398978" ID="ID_38980067" MODIFIED="1360083434976" TEXT="If name|Tid are used, return excepiton"/>
 </node>
 </node>
-<node CREATED="1357358147828" ID="ID_1186684411" MODIFIED="1357358159690" POSITION="right" TEXT="void worker()">
-<node CREATED="1357358162548" ID="ID_1797233738" MODIFIED="1357358180689" TEXT="on recept of work_packet, do the deed"/>
-<node CREATED="1357358805513" ID="ID_1740349485" MODIFIED="1357358832325" TEXT="on startup, send this.available to listener_tid"/>
-<node CREATED="1357358181756" ID="ID_12893510" MODIFIED="1357358939837" TEXT="when done, set work_packet.done = true and send to listener thread"/>
-<node CREATED="1357358614842" ID="ID_1012337941" MODIFIED="1357358636510" TEXT="on recept of this.close, terminate the thread"/>
 </node>
+<node CREATED="1360082136187" ID="ID_181135344" MODIFIED="1360082187227" POSITION="right" TEXT="task pool manager(t)(string name)"/>
+<node CREATED="1360081583633" ID="ID_1103044446" MODIFIED="1360082218064" POSITION="right" TEXT="worker thread(t)(Tid task_pool_manager)"/>
+<node CREATED="1360081590643" ID="ID_1324728" MODIFIED="1360081668955" POSITION="right" TEXT="struct work_packet(t)"/>
+<node CREATED="1360081723340" ID="ID_1492950840" MODIFIED="1360081762970" POSITION="right" TEXT="struct limits"/>
+<node CREATED="1360081739372" ID="ID_452972575" MODIFIED="1360081747882" POSITION="right" TEXT="struct run_ctl"/>
 </node>
 </map>
