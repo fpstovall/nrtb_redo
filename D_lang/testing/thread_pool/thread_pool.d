2d6aebc5f905192313d8cb864697c354ca9b8a4c
Rick Stovall <fpstovall>
2013-03-07 13:05:54 -0500

    Cleaned and enchanced pool example code. This was done to help refine the best way to structure a thread pool module.

--- D_lang/testing/thread_pool/thread_pool.d
+++ D_lang/testing/thread_pool/thread_pool.d
@@ -3,7 +3,7 @@ Multithreaded message passing example in D
 *********************/
 
 // import the concurrency and standard IO modules
-import std.concurrency, std.stdio, core.thread;
+import std.concurrency, std.stdio, core.thread, std.string;
 
 struct thread_pool(wp_t, alias task)
 {
@@ -11,8 +11,8 @@ struct thread_pool(wp_t, alias task)
   private Tid listener_tid;
   
   // Does nothing but start the listener.
-  public this(int queue_size) {
-    listener_tid = spawn(&listener_thread!(wp_t,task),queue_size);
+  public this(string name, int queue_size) {
+    listener_tid = spawn(&listener_thread!(wp_t,task),name,queue_size);
   };
     
   // submit a work, packet for processing.
@@ -22,17 +22,20 @@ struct thread_pool(wp_t, alias task)
       
 }
 
-void worker_thread(wpt, alias task)() {
+void worker_thread(wpt, alias task)(string id) {
+  writeln(id," started.");
   bool running = true;
+  int c = 0;
   while (running) {
     receive (
-      (Tid t, wpt d) { task(t,d); },
+      (Tid t, wpt d) { task(t,d); c++; },
       (OwnerTerminated e) { running = false; }
     );
   };
+  writeln(id," processed ",c," messages.");
 }
 
-void listener_thread(wpt, alias task)(int queue_size) {
+void listener_thread(wpt, alias task)(string name, int queue_size) {
   
   Tid worker_list[];
   ulong next = 0;
@@ -43,25 +46,28 @@ void listener_thread(wpt, alias task)(int queue_size) {
   }
   
   // initial setup.
+  writeln(name," listener started");
   for(int i = 0; i< queue_size; i++) {
-    worker_list ~= spawn(&worker_thread!(wpt,task));
+    string id = format("%s_worker_%d",name,i);
+    worker_list ~= spawn(&worker_thread!(wpt, task), id);
   }
     
   // service loop
   bool running = true;
+  int sent = 0;
   while (running) {
     receive ( 
-      (Tid t, wpt d) { submit(t,d); },
+      (Tid t, wpt d) { submit(t,d); sent++; },
       (OwnerTerminated e) { running = false; }
     );
   };
+  writeln(name," queued ",sent," messages to workers.");
 };
 
 //==== code starts here
 
 void mytask(Tid t, int i) {
-  writeln("Task ",thisTid," processed ",i);
-  Thread.sleep(dur!("msecs") (20));
+  Thread.sleep(dur!("msecs") (i));
 };
 
 
@@ -69,13 +75,12 @@ void mytask(Tid t, int i) {
 void main()
 {
   writeln("D Message Driven Work Queue Example.");
-  auto myqueue = thread_pool!(int,mytask)(10);
-  
+  auto myqueue = thread_pool!(int,mytask)("thread_pool",6);
+    
   for (auto i=0; i<100; i++) {
     myqueue.submit(i);
   };
   
-  Thread.sleep(dur!("seconds") (3));
-  writeln("Run complete");
-  
+  //Thread.sleep(dur!("seconds") (3));
+  writeln("All messages sent.");
 };
