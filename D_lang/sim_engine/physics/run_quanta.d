2895004e470a91e929cde49f1981f8f81f4759ea
Rick Stovall <fpstovall>
2013-03-25 20:49:01 -0400

    Factored out one iterative pass over the object list in run_quanta().

--- D_lang/sim_engine/physics/run_quanta.d
+++ D_lang/sim_engine/physics/run_quanta.d
@@ -41,30 +41,20 @@ pure void run_quanta(Tid t, ref current_status c, ref world w) {
   }
   
   // simple boundary sphere check for collisions
-  struct bumpers { object a, object b };
-  bumpers[] bumps;
   l = w.objects.length;
   for(auto i=0; i<l-1; i++) {
     a = w.objects[i];
     for (auto j=i+1; j<l; j++) {
       b = w.objects[j];
       if (a.position.magnitude(b.position) < (a.radius + b.radius)) {
-	// add this pair to the contact list.
-	bumpers t;
-	t.a = a;
-	t.b = b;
-	bumps ~= t;
+	// notify those involved in the collision.
+	impact i;
+	i.quanta = c.quanta;
+	i.impactor = a;
+	b.wrapper.send(i);
+	i.impactor = b;
+	a.wrapper.send(i);
       }
     }
-  }
-  
-  // send impact notifications.
-  foreach(auto b, bumps) {
-    impact i;
-    i.quanta = c.quanta;
-    i.impactor = b.a;
-    b.b.wrapper.send(i);
-    i.impactor = b.b;
-    b.a.wrapper.send(i);
   }
 }
