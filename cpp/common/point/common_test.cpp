7c97944fdb87d96348bd3288ddea228a2d949efd
Rick Stovall <fpstovall>
2013-07-05 20:24:44 -0400

    Reviewed, cleaned up and added the nrtb::point template back into the common library build. Unit tested and ready to go.

--- cpp/common/point/common_test.cpp
+++ cpp/common/point/common_test.cpp
@@ -32,8 +32,8 @@ int test_triad(const std::string prompt, ld_triad val, ld_triad right, int ec)
   cout << "\t" << prompt << " = " << val << endl;
   if (val != right)
   {
-	ec++;
-	cerr << "\t\tTest Failed: Answer should be " << val << endl;
+    ec++;
+    cerr << "\t\tTest Failed: Answer should be " << val << endl;
   }
   return ec;
 };
@@ -43,75 +43,75 @@ int test_ld(const std::string prompt, long double val, long double right, int ec
   cout << "\t" << prompt << " = " << val << endl;
   if (val != right)
   {
-	ec++;
-	cerr << "\t\tTest Failed: Answer should be " << val << endl;
+    ec++;
+    cerr << "\t\tTest Failed: Answer should be " << val << endl;
   }
   return ec;
 };
 
 int main()
 {
-	ld_triad a(1,2,3);
-	ld_triad b(3,2,1);
-	int returnme = 0;
+  ld_triad a(1,2,3);
+  ld_triad b(3,2,1);
+  int returnme = 0;
 
-	cout << setprecision(10);
-	cout << "=== nrtb::triad Unit Test ===" << endl;
-	cout << "\ta = " << a << "; b = " << b << endl;
-	// basic operations tests
-	returnme = test_triad("a + b",a + b,ld_triad(4,4,4),returnme);
-	returnme = test_triad("a - b",a - b,ld_triad(-2,0,2),returnme);
-	returnme = test_triad("a * b",a * b,ld_triad(3,4,3),returnme);
-	returnme = test_triad("a / b",a / b,ld_triad(1.0d/(long double) 3.0,1,3),returnme);
-	returnme = test_triad("a += b; a",a += b,ld_triad(4,4,4),returnme);
-	returnme = test_triad("a -= b; a",a -= b,ld_triad(1,2,3),returnme);
-	returnme = test_triad("a *= b; a",a *= b,ld_triad(3,4,3),returnme);
-	returnme = test_triad("a /= b; a",a /= b,ld_triad(1,2,3),returnme);
-	// power test
-	returnme = test_triad("a.pow(b)",a.pow(b),ld_triad(1,4,3),returnme);
-	// range test
-	ld_triad t = b - a;
-	t *= t;
-	long double r = sqrt(t.x + t.y + t.z);
-	returnme = test_ld("a.range(b)",a.range(b),r,returnme);
-	// magnatude test
-	returnme = test_ld("a.magnatude()",a.magnatude(),a.range(0),returnme);
-	// boolean tests
-	returnme = test_ld("a == a",a == a,1,returnme);
-	returnme = test_ld("a == b",a == b,0,returnme);
-	returnme = test_ld("a != b",a != b,1,returnme);
-	returnme = test_ld("a != a",a != a,0,returnme);
-	// point/scalar operations
-	returnme = test_triad("a + 2",a + 2,ld_triad(3,4,5),returnme);
-	returnme = test_triad("a - 2",a - 2,ld_triad(-1,0,1),returnme);
-	returnme = test_triad("a * 2",a * 2,ld_triad(2,4,6),returnme);
-	returnme = test_triad("a / 2",a / 2,ld_triad(0.5,1,1.5),returnme);
-	returnme = test_triad("a += 2",a += 2,ld_triad(3,4,5),returnme);
-	returnme = test_triad("a -= 2",a -= 2,ld_triad(1,2,3),returnme);
-	returnme = test_triad("a *= 2",a *= 2,ld_triad(2,4,6),returnme);
-	returnme = test_triad("a /= 2",a /= 2,ld_triad(1,2,3),returnme);
-	returnme = test_triad("a.pow(2)",a.pow(2),ld_triad(1,4,9),returnme);
-	// normalization test
-	cout << "\ta.normalize() = " << a.normalize() << endl;
-	returnme = test_ld("a.normalize().magnatude()",a.normalize().magnatude(),1.0,returnme);
-	// dot and vector product tests.
-	returnme = test_ld("a.dot_product(b)",a.dot_product(b),10,returnme);
-	returnme = test_triad("a.vector_product(b)",a.vector_product(b),
-		ld_triad(-4,8,-4),returnme);
-	// string i/o tests, assumes "2,3.5,7) is input.
-	cout << "\tInput a new value for b \"(2,3.5,7)\": " << flush;
-	cin >> b; cout << endl;
-	returnme = test_triad("b",b,ld_triad(2,3.5,7),returnme);
-	returnme = test_triad("b.from_str(b.to_str(10))",
-					b.from_str(b.to_str(10)),ld_triad(2,3.5,7),returnme);
-	// report errors, if any
-	if (returnme)
-	{
-	  cerr << "There were " << returnme 
-		<< " error(s) found." << endl;
-	}
-	cout << "=== nrtb::triad Unit Test Complete ===" << endl;
-	// return the error count as the exit code
-	return returnme;
+  cout << setprecision(10);
+  cout << "=== nrtb::triad Unit Test ===" << endl;
+  cout << "\ta = " << a << "; b = " << b << endl;
+  // basic operations tests
+  returnme = test_triad("a + b",a + b,ld_triad(4,4,4),returnme);
+  returnme = test_triad("a - b",a - b,ld_triad(-2,0,2),returnme);
+  returnme = test_triad("a * b",a * b,ld_triad(3,4,3),returnme);
+  returnme = test_triad("a / b",a / b,ld_triad(1.0d/(long double) 3.0,1,3),returnme);
+  returnme = test_triad("a += b; a",a += b,ld_triad(4,4,4),returnme);
+  returnme = test_triad("a -= b; a",a -= b,ld_triad(1,2,3),returnme);
+  returnme = test_triad("a *= b; a",a *= b,ld_triad(3,4,3),returnme);
+  returnme = test_triad("a /= b; a",a /= b,ld_triad(1,2,3),returnme);
+  // power test
+  returnme = test_triad("a.pow(b)",a.pow(b),ld_triad(1,4,3),returnme);
+  // range test
+  ld_triad t = b - a;
+  t *= t;
+  long double r = sqrt(t.x + t.y + t.z);
+  returnme = test_ld("a.range(b)",a.range(b),r,returnme);
+  // magnatude test
+  returnme = test_ld("a.magnatude()",a.magnatude(),a.range(0),returnme);
+  // boolean tests
+  returnme = test_ld("a == a",a == a,1,returnme);
+  returnme = test_ld("a == b",a == b,0,returnme);
+  returnme = test_ld("a != b",a != b,1,returnme);
+  returnme = test_ld("a != a",a != a,0,returnme);
+  // point/scalar operations
+  returnme = test_triad("a + 2",a + 2,ld_triad(3,4,5),returnme);
+  returnme = test_triad("a - 2",a - 2,ld_triad(-1,0,1),returnme);
+  returnme = test_triad("a * 2",a * 2,ld_triad(2,4,6),returnme);
+  returnme = test_triad("a / 2",a / 2,ld_triad(0.5,1,1.5),returnme);
+  returnme = test_triad("a += 2",a += 2,ld_triad(3,4,5),returnme);
+  returnme = test_triad("a -= 2",a -= 2,ld_triad(1,2,3),returnme);
+  returnme = test_triad("a *= 2",a *= 2,ld_triad(2,4,6),returnme);
+  returnme = test_triad("a /= 2",a /= 2,ld_triad(1,2,3),returnme);
+  returnme = test_triad("a.pow(2)",a.pow(2),ld_triad(1,4,9),returnme);
+  // normalization test
+  cout << "\ta.normalize() = " << a.normalize() << endl;
+  returnme = test_ld("a.normalize().magnatude()",a.normalize().magnatude(),1.0,returnme);
+  // dot and vector product tests.
+  returnme = test_ld("a.dot_product(b)",a.dot_product(b),10,returnme);
+  returnme = test_triad("a.vector_product(b)",a.vector_product(b),
+	  ld_triad(-4,8,-4),returnme);
+  // string i/o tests, assumes "2,3.5,7) is input.
+  cout << "\tInput a new value for b \"(2,3.5,7)\": " << flush;
+  cin >> b; cout << endl;
+  returnme = test_triad("b",b,ld_triad(2,3.5,7),returnme);
+  returnme = test_triad("b.from_str(b.to_str(10))",
+				  b.from_str(b.to_str(10)),ld_triad(2,3.5,7),returnme);
+  // report errors, if any
+  if (returnme)
+  {
+    cerr << "There were " << returnme 
+      << " error(s) found." << endl;
+  }
+  cout << "=== nrtb::triad Unit Test Complete ===" << endl;
+  // return the error count as the exit code
+  return returnme;
 	
 };
--- cpp/common/point/triad.h
+++ cpp/common/point/triad.h
@@ -43,265 +43,265 @@ namespace nrtb
 template <class T> 
 struct triad
 {
-	/// X coordinate
-	T x;
-	/// Y coordinate
-	T y;
-	/// Z coordinate
-	T z;
+  /// X coordinate
+  T x;
+  /// Y coordinate
+  T y;
+  /// Z coordinate
+  T z;
 
-	/** Constructor. 
-	 ** 
-	 ** An easy way to create a point at the origin would be 
-	 ** "triad<T> a;".
-	 **/
-	triad(T _x=0, T _y=0, T _z=0);
-	triad<T> operator + (const triad<T> & a);
-	triad<T> operator - (const triad<T> & a);
-	triad<T> operator * (const triad<T> & a);
-	triad<T> operator / (const triad<T> & a);
-	triad<T> operator += (const triad<T> & a);
-	triad<T> operator -= (const triad<T> & a);
-	triad<T> operator *= (const triad<T> & a);
-	triad<T> operator /= (const triad<T> & a);
-	/// returns (pow(x,a.x),pow(y,a.y),pow(z,a.z)).
-	triad<T> pow(const triad<T> & a);
-	triad<T> operator + (const T & a);
-	triad<T> operator - (const T & a);
-	triad<T> operator * (const T & a);
-	triad<T> operator / (const T & a);
-	triad<T> operator += (const T & a);
-	triad<T> operator -= (const T & a);
-	triad<T> operator *= (const T & a);
-	triad<T> operator /= (const T & a);
-	/// returns (pow(x,a),pow(y,a),pow(z,a)).
-	triad<T> pow(const T & a);
-	/** Returns a normalized version of the triad.
-	 ** 
-	 ** Normalization in this case refers to an operation resulting in a
-	 ** traid where the magnatude of the vector is 1.0 while maintaining the
-	 ** same angular component. For example, given triad<float> a = (1,2,3),
-	 ** a.normalize() = (0.267,0.534,0.802). Of course, the original triad
-	 ** is not modified.
-	 ** 
-	 ** Normalized triads are very useful when calcuating force or
-	 ** accelleration vectors, for example.
-	 **/
-	triad<T> normalize();
-	/// Returns the distance between *this and the supplied argument.
-	T range(const triad<T> & a);
-	/// Returns the magnatude of the vector.
-	T magnatude();
-	/// Returns the dot (scalar) product of two triads
-	T dot_product(const triad<T> & a);
-	/// Returns the vector product of two triads
-	triad<T> vector_product(const triad<T> & a);
-	bool operator == (const triad<T> & a);
-	bool operator != (const triad<T> & a);
-	/// Loads from a std::string.
-	triad<T> from_str(const std::string & a);
-	/// Returns a std::string representation.
-	std::string to_str(int precision=6);
+  /** Constructor. 
+    ** 
+    ** An easy way to create a point at the origin would be 
+    ** "triad<T> a;".
+    **/
+  triad(T _x=0, T _y=0, T _z=0);
+  triad<T> operator + (const triad<T> & a);
+  triad<T> operator - (const triad<T> & a);
+  triad<T> operator * (const triad<T> & a);
+  triad<T> operator / (const triad<T> & a);
+  triad<T> operator += (const triad<T> & a);
+  triad<T> operator -= (const triad<T> & a);
+  triad<T> operator *= (const triad<T> & a);
+  triad<T> operator /= (const triad<T> & a);
+  /// returns (pow(x,a.x),pow(y,a.y),pow(z,a.z)).
+  triad<T> pow(const triad<T> & a);
+  triad<T> operator + (const T & a);
+  triad<T> operator - (const T & a);
+  triad<T> operator * (const T & a);
+  triad<T> operator / (const T & a);
+  triad<T> operator += (const T & a);
+  triad<T> operator -= (const T & a);
+  triad<T> operator *= (const T & a);
+  triad<T> operator /= (const T & a);
+  /// returns (pow(x,a),pow(y,a),pow(z,a)).
+  triad<T> pow(const T & a);
+  /** Returns a normalized version of the triad.
+    ** 
+    ** Normalization in this case refers to an operation resulting in a
+    ** traid where the magnatude of the vector is 1.0 while maintaining the
+    ** same angular component. For example, given triad<float> a = (1,2,3),
+    ** a.normalize() = (0.267,0.534,0.802). Of course, the original triad
+    ** is not modified.
+    ** 
+    ** Normalized triads are very useful when calcuating force or
+    ** accelleration vectors, for example.
+    **/
+  triad<T> normalize();
+  /// Returns the distance between *this and the supplied argument.
+  T range(const triad<T> & a);
+  /// Returns the magnatude of the vector.
+  T magnatude();
+  /// Returns the dot (scalar) product of two triads
+  T dot_product(const triad<T> & a);
+  /// Returns the vector product of two triads
+  triad<T> vector_product(const triad<T> & a);
+  bool operator == (const triad<T> & a);
+  bool operator != (const triad<T> & a);
+  /// Loads from a std::string.
+  triad<T> from_str(const std::string & a);
+  /// Returns a std::string representation.
+  std::string to_str(int precision=6);
 };
 
 template <class T>
 triad<T>::triad(T _x, T _y, T _z)
 {
-	x = _x;
-	y = _y;
-	z = _z;
+  x = _x;
+  y = _y;
+  z = _z;
 };
 
 template <class T>
 triad<T> triad<T>::operator + (const triad<T> & a)
 {
-	triad<T> returnme;
-	returnme.x = x + a.x;
-	returnme.y = y + a.y;
-	returnme.z = z + a.z;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x + a.x;
+  returnme.y = y + a.y;
+  returnme.z = z + a.z;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator - (const triad<T> & a)
 {
-	triad<T> returnme;
-	returnme.x = x - a.x;
-	returnme.y = y - a.y;
-	returnme.z = z - a.z;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x - a.x;
+  returnme.y = y - a.y;
+  returnme.z = z - a.z;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator * (const triad<T> & a)
 {
-	triad<T> returnme;
-	returnme.x = x * a.x;
-	returnme.y = y * a.y;
-	returnme.z = z * a.z;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x * a.x;
+  returnme.y = y * a.y;
+  returnme.z = z * a.z;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator / (const triad<T> & a)
 {
-	triad<T> returnme;
-	returnme.x = x / a.x;
-	returnme.y = y / a.y;
-	returnme.z = z / a.z;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x / a.x;
+  returnme.y = y / a.y;
+  returnme.z = z / a.z;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator += (const triad<T> & a)
 {
-	x += a.x;
-	y += a.y;
-	z += a.z;
-	return *this;
+  x += a.x;
+  y += a.y;
+  z += a.z;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator -= (const triad<T> & a)
 {
-	x -= a.x;
-	y -= a.y;
-	z -= a.z;
-	return *this;
+  x -= a.x;
+  y -= a.y;
+  z -= a.z;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator *= (const triad<T> & a)
 {
-	x *= a.x;
-	y *= a.y;
-	z *= a.z;
-	return *this;
+  x *= a.x;
+  y *= a.y;
+  z *= a.z;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator /= (const triad<T> & a)
 {
-	x /= a.x;
-	y /= a.y;
-	z /= a.z;
-	return *this;
+  x /= a.x;
+  y /= a.y;
+  z /= a.z;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::pow(const triad<T> & a)
 {
-	triad<T> returnme;
-	returnme.x = ::pow(x,a.x);
-	returnme.y = ::pow(y,a.y);
-	returnme.z = ::pow(z,a.z);
-	return returnme;
+  triad<T> returnme;
+  returnme.x = ::pow(x,a.x);
+  returnme.y = ::pow(y,a.y);
+  returnme.z = ::pow(z,a.z);
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::normalize()
 {
-	return (*this / this->magnatude());
+  return (*this / this->magnatude());
 };
 
 template <class T>
 triad<T> triad<T>::operator + (const T & a)
 {
-	triad<T> returnme;
-	returnme.x = x + a;
-	returnme.y = y + a;
-	returnme.z = z + a;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x + a;
+  returnme.y = y + a;
+  returnme.z = z + a;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator - (const T & a)
 {
-	triad<T> returnme;
-	returnme.x = x - a;
-	returnme.y = y - a;
-	returnme.z = z - a;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x - a;
+  returnme.y = y - a;
+  returnme.z = z - a;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator * (const T & a)
 {
-	triad<T> returnme;
-	returnme.x = x * a;
-	returnme.y = y * a;
-	returnme.z = z * a;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x * a;
+  returnme.y = y * a;
+  returnme.z = z * a;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator / (const T & a)
 {
-	triad<T> returnme;
-	returnme.x = x / a;
-	returnme.y = y / a;
-	returnme.z = z / a;
-	return returnme;
+  triad<T> returnme;
+  returnme.x = x / a;
+  returnme.y = y / a;
+  returnme.z = z / a;
+  return returnme;
 };
 
 template <class T>
 triad<T> triad<T>::operator += (const T & a)
 {
-	x += a;
-	y += a;
-	z += a;
-	return *this;
+  x += a;
+  y += a;
+  z += a;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator -= (const T & a)
 {
-	x -= a;
-	y -= a;
-	z -= a;
-	return *this;
+  x -= a;
+  y -= a;
+  z -= a;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator *= (const T & a)
 {
-	x *= a;
-	y *= a;
-	z *= a;
-	return *this;
+  x *= a;
+  y *= a;
+  z *= a;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::operator /= (const T & a)
 {
-	x /= a;
-	y /= a;
-	z /= a;
-	return *this;
+  x /= a;
+  y /= a;
+  z /= a;
+  return *this;
 };
 
 template <class T>
 triad<T> triad<T>::pow(const T & a)
 {
-	triad<T> returnme;
-	returnme.x = ::pow(x,a);
-	returnme.y = ::pow(y,a);
-	returnme.z = ::pow(z,a);
-	return returnme;
+  triad<T> returnme;
+  returnme.x = ::pow(x,a);
+  returnme.y = ::pow(y,a);
+  returnme.z = ::pow(z,a);
+  return returnme;
 };
 
 template <class T>
 T triad<T>::range(const triad<T> & a)
 {
-	triad<T> offset = *this - a;
-	offset *= offset;
-	return sqrt(offset.x + offset.y + offset.z);
+  triad<T> offset = *this - a;
+  offset *= offset;
+  return sqrt(offset.x + offset.y + offset.z);
 };
 
 template <class T>
 T triad<T>::magnatude()
 {
-	return sqrt((x*x) + (y*y) + (z*z));
+  return sqrt((x*x) + (y*y) + (z*z));
 };
 
 template <class T>
@@ -327,39 +327,39 @@ triad<T> triad<T>::vector_product(const triad<T> & a)
 template <class T>
 bool triad<T>::operator == (const triad<T> & a)
 {
-	return ((x == a.x) && (y == a.y) && (z == a.z));
+  return ((x == a.x) && (y == a.y) && (z == a.z));
 };
 
 template <class T>
 bool triad<T>::operator != (const triad<T> & a)
 {
-	return (! (*this == a));
+  return (! (*this == a));
 };
 
 template <class T>
 triad<T> triad<T>::from_str(const std::string & a)
 {
-	std::istringstream t(a.c_str());
-	t >> *this;
-	return *this;
+  std::istringstream t(a.c_str());
+  t >> *this;
+  return *this;
 };
 
 template <class T>
 std::string triad<T>::to_str(int precision)
 {
-	std::stringstream a;
-	a << std::setprecision(precision) << *this;
-	std::string returnme;
-	a >> returnme;
-	return returnme;
+  std::stringstream a;
+  a << std::setprecision(precision) << *this;
+  std::string returnme;
+  a >> returnme;
+  return returnme;
 };
 
 /// Writes the triad<T> in the format "(x,y,z)" to the output stream.
 template <class T>
 std::ostream & operator << (std::ostream &f, const triad<T> & a)
 {
-	f << "(" << a.x << "," << a.y << "," << a.z << ")";
-	return f;
+  f << "(" << a.x << "," << a.y << "," << a.z << ")";
+  return f;
 };
 
 /** Reads the triad<T> from an input stream.
@@ -372,22 +372,22 @@ std::ostream & operator << (std::ostream &f, const triad<T> & a)
 template <class T>
 std::istream & operator >> (std::istream &f, triad<T> & a)
 {
-	std::string element;
-	f >> element;
-	strlist t = split(element,',');
-	if (t.size() != 3)
-	{
-		a = 0;
-	}
-	else
-	{
-		std::stringstream transmorgrafier;
-		transmorgrafier << std::setprecision(30);
-		if (t[0][0] == '(') { t[0].erase(0,1); };
-		for(int i=0; i < 3; i++) { transmorgrafier << t[i] << " "; };
-		transmorgrafier >> a.x >> a.y >> a.z;
-	};
-	return f;
+  std::string element;
+  f >> element;
+  strlist t = split(element,',');
+  if (t.size() != 3)
+  {
+    a = 0;
+  }
+  else
+  {
+    std::stringstream transmorgrafier;
+    transmorgrafier << std::setprecision(30);
+    if (t[0][0] == '(') { t[0].erase(0,1); };
+    for(int i=0; i < 3; i++) { transmorgrafier << t[i] << " "; };
+    transmorgrafier >> a.x >> a.y >> a.z;
+  };
+  return f;
 };
 
 } // namespace nrtb
