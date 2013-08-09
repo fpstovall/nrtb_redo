ba81e579ced079742b835caa46a25c6e03c01247
Rick Stovall <fpstovall>
2013-08-09 10:34:48 -0400

    Completed conf_reader cleanup and revised unit test. Removed the singleton version as problematic, altered logging to use the new logger class. Unit tested and ready to use.

--- cpp/common/confreader/confreader.cpp
+++ cpp/common/confreader/confreader.cpp
@@ -21,206 +21,203 @@
 #include "confreader.h"
 #include <fstream>
 #include <iostream>
-#include <common.h>
-#include "Poco/Logger.h"
 
 using namespace std;
 using namespace nrtb;
 
-const std::string logname = "conf_reader";
-
 namespace nrtb
 {
 
-conf_reader::conf_reader()
+conf_reader::conf_reader(std::string name, log_queue & l)
+  : logger(name, l)
 {
-	Poco::Logger& logger = Poco::Logger::get(logname);
-	logger.information("conf_reader instanciated.");
+  logger.trace("Instaciated");
 };
 
-conf_reader::~conf_reader() {};
+conf_reader::~conf_reader() 
+{
+  logger.trace("Destructed");
+};
 
 unsigned int conf_reader::read(const std::string & _filename, bool _clear)
 {
-	Poco::Logger& logger = Poco::Logger::get(logname);
-	logger.information("Reading from \"" + _filename + "\".");
-	if (_filename != "") { filename = _filename; };
-	if (filename != "")
+  logger.info("Reading "+_filename);
+  if (_filename != "") { filename = _filename; };
+  if (filename != "")
+  {
+    try
+    {
+      ifstream cfile(filename.c_str());
+      if (!cfile) throw base_exception();
+      if (_clear) values.clear();
+      // read the file line by line, ignoring comments.
+      char inbuff[16*1024];
+      while (cfile)
+      {
+	cfile.getline(inbuff, 16*1024);
+	string in = inbuff;
+	// truncate at the start of a comment.
+	unsigned long int where = in.find("#");
+	while (where != string::npos)
 	{
-		try
-		{
-			ifstream cfile(filename.c_str());
-			if (!cfile) throw base_exception();
-			if (_clear) values.clear();
-			// read the file line by line, ignoring comments.
-			char inbuff[16*1024];
-			while (cfile)
-			{
-				cfile.getline(inbuff, 16*1024);
-				string in = inbuff;
-				// truncate at the start of a comment.
-				unsigned long int where = in.find("#");
-				while (where != string::npos)
-				{
-					if (in[where-1] == '\\')
-					{
-						// comment char was escaped.. ignore.
-						where = in.find("#",where+1);
-					}
-					else
-					{
-						// truncate the string at this point.
-						in.erase(where);
-						where = string::npos;
-					};
-				};
-				// see if we can parse what's left.
-				in = trim(in);
-				unsigned long int split_point = in.find_first_of("\t ");
-				if (split_point != string::npos)
-				{
-					// okay.. get the fields.
-					pair arg;
-					arg.first = gsub(trim(in.substr(0,split_point)),"\\#","#");
-					arg.second = gsub(trim(in.substr(split_point)),"\\#","#");
-					// is this an include directive?
-					if (arg.first == "*INCLUDE")
-					{
-						read(arg.second,false);
-					} 
-					else if (arg.first != "")
-					{
-						values.insert(arg);
-					};
-				};
-			};
-		}
-		catch (...)
-		{
-			logger.warning("Problems reading configuration file \""
-				+ filename + "\"; data may be incomplete.");
-		}
+	  if (in[where-1] == '\\')
+	  {
+	    // comment char was escaped.. ignore.
+	    where = in.find("#",where+1);
+	  }
+	  else
+	  {
+	    // truncate the string at this point.
+	    in.erase(where);
+	    where = string::npos;
+	  };
 	};
-	return values.size();
+	// see if we can parse what's left.
+	in = trim(in);
+	unsigned long int split_point = in.find_first_of("\t ");
+	if (split_point != string::npos)
+	{
+	  // okay.. get the fields.
+	  pair arg;
+	  arg.first = gsub(trim(in.substr(0,split_point)),"\\#","#");
+	  arg.second = gsub(trim(in.substr(split_point)),"\\#","#");
+	  // is this an include directive?
+	  if (arg.first == "*INCLUDE")
+	  {
+	    read(arg.second,false);
+	  } 
+	  else if (arg.first != "")
+	  {
+	    values.insert(arg);
+	  };
+	};
+      };
+    }
+    catch (...)
+    {
+      logger.warning("Problems reading configuration file \""
+	      + filename + "\"; data may be incomplete.");
+    }
+  };
+  return values.size();
 };
 
 unsigned int conf_reader::read(int argc, char * argv[], 
 	const string & _filename)
 {
-	Poco::Logger& logger = Poco::Logger::get(logname);
-	logger.information("Reading from command line.");
-	clear();
-	filename = _filename;
-	value_list_type cvars;
-	// read values from the command line first.
-	for (int i = 0; i < argc; i++)
+  logger.info("Reading from command line");
+  clear();
+  filename = _filename;
+  value_list_type cvars;
+  // read values from the command line first.
+  for (int i = 0; i < argc; i++)
+  {
+    string instring = argv[i];
+    if (i == 0)
+    {
+      instring = "__exec_name="+instring;
+    };
+    strlist t = split(instring,'=');
+    if (t.size() > 0)
+    {
+      // build the new insert pair;
+      pair newval;
+      newval.first = t[0];
+      // assemble the value
+      // (allows for including "=" in the argument)
+      for (unsigned int l = 1; l < t.size(); l++)
+      {
+	newval.second += t[l];
+	if (l < (t.size() -1) )
 	{
-		string instring = argv[i];
-		if (i == 0)
-		{
-			instring = "__exec_name="+instring;
-		};
-		strlist t = split(instring,'=');
-		if (t.size() > 0)
-		{
-			// build the new insert pair;
-			pair newval;
-			newval.first = t[0];
-			// assemble the value
-			// (allows for including "=" in the argument)
-			for (unsigned int l = 1; l < t.size(); l++)
-			{
-				newval.second += t[l];
-				if (l < (t.size() -1) )
-				{
-					newval.second += "=";
-				};
-			};
-			// store this in the list
-			trim(newval.first);
-			trim(newval.second);
-			cvars.insert(newval);
-			// is this a config file name?
-			if (newval.first == "configfile")
-			{
-				filename = newval.second;
-			};
-		};
-	}; // read the command line arguments.
-	// read the file args if any.
-	read(filename,false);
-	// override the first instance of any value found in configs
-	// or insert as appropriate.
-	iterator c = cvars.begin();
-	iterator e = cvars.end();
-	while (c != e)
-	{
-		iterator here = values.find(c->first);
-		if (here != values.end())
-		{
-			here->second = c->second;
-		}
-		else
-		{
-			values.insert(*c);
-		};
-		c++;
+	  newval.second += "=";
 	};
-	std::stringstream message;
-	message << "Read " << values.size() << " parameters.";
-	logger.information(message.str());
-	return values.size();
+      };
+      // store this in the list
+      trim(newval.first);
+      trim(newval.second);
+      cvars.insert(newval);
+      // is this a config file name?
+      if (newval.first == "configfile")
+      {
+	filename = newval.second;
+      };
+    };
+  }; // read the command line arguments.
+  // read the file args if any.
+  read(filename,false);
+  // override the first instance of any value found in configs
+  // or insert as appropriate.
+  iterator c = cvars.begin();
+  iterator e = cvars.end();
+  while (c != e)
+  {
+    iterator here = values.find(c->first);
+    if (here != values.end())
+    {
+      here->second = c->second;
+    }
+    else
+    {
+      values.insert(*c);
+    };
+    c++;
+  };
+  std::stringstream message;
+  message << "Read " << values.size() << " parameters.";
+  logger.info(message.str());
+  return values.size();
 };
 
 void conf_reader::clear()
 {
-	values.clear();
+  values.clear();
 };
 
 conf_reader::iterator conf_reader::begin()
 {
-	return values.begin();
+  return values.begin();
 };
 
 conf_reader::iterator conf_reader::end()
 {
-	return values.end();
+  return values.end();
 };
 
 bool conf_reader::empty()
 {
-	return values.empty();
+  return values.empty();
 };
 
 unsigned int conf_reader::size()
 {
-	return values.size();
+  return values.size();
 };
 
 strlist conf_reader::all(const std::string & key)
 {
-	strlist returnme;
-	iterator current = values.find(key);
-	iterator e = values.end();
-	while ((current != e) && (current->first == key))
-	{
-		returnme.push_back(current->second);
-		current++;
-	};
-	return returnme;
+  strlist returnme;
+  iterator current = values.find(key);
+  iterator e = values.end();
+  while ((current != e) && (current->first == key))
+  {
+    returnme.push_back(current->second);
+    current++;
+  };
+  return returnme;
 };
 
 string conf_reader::operator [] (const std::string & key)
 {
-	iterator res = values.find(key);
-	string returnme = "";
-	if (res != values.end()) returnme = res->second;
-	return returnme;
+  iterator res = values.find(key);
+  string returnme = "";
+  if (res != values.end()) returnme = res->second;
+  return returnme;
 };
 
 bool conf_reader::exists(const std::string & key)
 {
-	return (values.find(key) != values.end());
+  return (values.find(key) != values.end());
 };
 
 } // namespace nrtb
--- cpp/common/confreader/confreader.h
+++ cpp/common/confreader/confreader.h
@@ -16,8 +16,8 @@
  
  **********************************************/
 
-#ifndef ricklib_confreader_h
-#define ricklib_confreader_h
+#ifndef nrtb_confreader_h
+#define nrtb_confreader_h
 
 #include <string.h>
 #include <map>
@@ -25,12 +25,11 @@
 #include <string>
 #include <fstream>
 #include <boost/lexical_cast.hpp>
-#include <singleton.h>
+#include <common.h>
+#include <logger.h>
 
-namespace nrtb
+namespace nrtb 
 {
-
-typedef std::vector<std::string> strlist;
 
 /** Reads command line and configuration file information.
  **
@@ -47,241 +46,241 @@ typedef std::vector<std::string> strlist;
  **/
 class conf_reader
 {
-	private:
-		typedef std::multimap<std::string,std::string> value_list_type;
-		value_list_type values;
-		std::string filename;
-	protected:
-		/** Reads and parses a configuration file.
-		 ** 
-		 ** Returns the number of values stored.
-		 ** 
-		 ** The optional parameter _clear when true clears the currently
-		 ** stored list of values. if false the new values read will be added
-		 ** to the existing list.
-		 ** 
-		 ** If the file named can not be processed completely, a warning
-		 ** message is published to cerr. 
-		 ** 
-		 ** Configuration files are defined as follows.
-		 ** 
-		 ** 1. Files are read line by line. No value can cross a newline
-		 **    (and at least in this version) or include one. The good
-		 **    news is that you are allowed up to 16k for each line.
-		 **    
-		 ** 2. values are specifed in name/value pairs, one per line, with
-		 ** 	 the name first, followed by whitespace, followed by the 
-		 ** 	 value. Values may include whitespace. All leading and trailing
-		 ** 	 whitespace is removed from both the name and the value.
-		 ** 	 
-		 ** 3. Comments start with a \# symbol; all text following a \# is
-		 **    ignored. If you need to use \# in a name or value, escape
-		 **    it with a backslash instead.
-		 **    
-		 ** 4. Duplicate names are allowed.
-		 ** 
-		 ** 5. Names without values will be stored with "" as the value.
-		 ** 
-		 ** 6. A configuration file may include another configuration file
-		 **    automatically by the use of the "*INCLUDE" reserved name.
-		 **    When this name is found, it's value is used as the name of
-		 **    the new file to be read.
-		 **/
-		unsigned int read(const std::string & _filename = "", bool _clear=true);
-	public:
-		/// conf_reader iterator, points to a conf_reader::pair
-		typedef value_list_type::iterator iterator;
-		/// conf_reader::pair is a std::pair<string,string>
-		typedef std::pair<std::string,std::string> pair;
-		/** No argument constructor; actually calls read() without a filename.
-		**/
-		conf_reader();
-		/** NOP virtual destructor to allow safe inheritance.
-		 **/
-		virtual ~conf_reader();
-		/** Reads and parses the command line and the provided file.
-		 ** 
-		 ** Returns the number of values stored. argc and argv are, of 
-		 ** course the same names you used as arguments to main(). Therefore
-		 ** you can easily read all command line and configuration file 
-		 ** values like this:
-		 ** 
-		 ** int main(int argc, char* argv[])
-		 **
-		 ** {
-		 **
-		 ** 	conf_reader config;
-		 **
-		 ** 	config.read(argc,argv,"my_conf_file");
-		 **
-		 ** 	...
-		 **
-		 ** }
-		 ** 
-		 ** See read(filename,_clear) for a discription of how configuration
-		 ** files are structured and parsed. This method unconditionally
-		 ** clears the existing value list before starting. For command line
-		 ** arguments the following rules are true.
-		 ** 
-		 ** 1. The executable name (argv[0]) is stored as a value with the 
-		 **    name "__exec_name".
-		 **   
-		 ** 2. Command line arguments of the form "name=value" are parsed
-		 **    stored as named values.
-		 ** 
-		 ** 3. All other command line arguments are stored as names with 
-		 **    value = "".
-		 **    
-		 ** 4. A command line argument of form "configfile=filename" will
-		 **    override the filename supplied as an argument to this method.
-		 **    
-		 ** 5. In the case of duplicate command line arguments, the last one 
-		 **    specified wins.
-		 **    
-		 ** 6. In the case of names in the configuration file duplicating 
-		 **    names from the command line, the values from the command line
-		 **    dominate. If there were multiple values for a given name 
-		 **    specified in the file, only the first one (the one returned by
-		 **    the "[]" operator or get<>() method) is overridden.
-		 **/
-		unsigned int read(int argc, char* argv[], 
-			const std::string & _filename);
-		/// clears the name/value list.
-		void clear();
-		/// returns an iterator to the first item in the list.
-		iterator begin();
-		/// returns an iterator one past the end of the list.
-		iterator end();
-		/// True if there are no values, false otherwise.
-		bool empty();
-		/// Returns the number of values stored.
-		unsigned int size();
-		/** Returns the string value matching the supplied name.
-		 ** 
-		 ** NOTE: the use of get<T>(key) or get<T>(key,default) is preferred.
-		 ** This method is public to allow for specialized handling in the
-		 ** rare cases where it may be required.
-		 ** 
-		 ** If there are no matching names, "" is returned. Be aware that
-		 ** "" is a valid value, so you can not use this to verify that
-		 ** a given name was defined. Use exists() for that.
-		 **/
-		std::string operator [] (const std::string & key);
-		/** Returns all values associated with the supplied name.
-		 ** 
-		 ** NOTE: the use of getall<T>(key) is preferred. This method is 
-		 ** public to allow specialized handling in rare case where it
-		 ** may be required.
-		 ** 
-		 ** If there are no values defined  the strlist will be empty.
-		 **/
-		strlist all(const std::string & key);
-		/// True if the name exists, false otherwise.
-		bool exists(const std::string & key);
-		/** Use this to get all matching values.
-		 ** 
-		 ** Usage:
-		 ** 
-		 ** vector<type> mylist = conf_reader_object.getall<type>(key);
-		 ** 
-		 ** type can be any standard type (string, int, double, etc.) or any
-		 ** type for which the ">>" stream operator is defined. All values
-		 ** with matching names that can map to the requested type will be
-		 ** returned. Any that do not map will not be returned.
-		 **/
-		template < class T >
-			typename std::vector<T> getall(const std::string & key);
-		/** Use this to get the matching value.
-		 ** 
-		 ** Useage:
-		 ** 
-		 ** type myvar = get<type>(key);
-		 ** 
-		 ** type can be any standard type (string, int, double, etc.) or any
-		 ** type for which the ">>" stream operator is defined. The return
-		 ** value is initialized to all zeros if no matching name is found or
-		 ** if the first value with that name does not map to the requested 
-		 ** type. For the numeric types that results in zero being returned,
-		 ** but be aware that more complex object may be in non-sensible
-		 ** states if they were not found or could not map. Use exists() to 
-		 ** verify the existance of a given name if needed.
-		 **/
-		template < class T >
-			T get(const std::string & key);
-		/** Returns the value for the requested key, or the supplied default.
-		 ** 
-		 ** Works exactly like get<>() with the exception that if the key
-		 ** is not found in the list, the user supplied default value is
-		 ** returned instead of 0 or an empty string. If the value exists but
-		 ** can not map to the requested type, 0 or an empty string is 
-		 ** returned for the standard types.
-		 **/
-		template <class T>
-			T get(const std::string & key, const T & def);
-};
-
-typedef singleton<conf_reader> global_conf_reader;
-
-template < class T >
-	typename std::vector<T> conf_reader::getall(const std::string & key)
-{
-	strlist tvals = all(key);
-	std::vector<T> returnme;
-	if (typeid(T) == typeid(std::string))
-	{
-		// T is a std::string.. we can do this quickly.
-		strlist * wl = (strlist *) &returnme;
-		*wl = tvals;
-	}
-	else
-	{
-		// T is non-string.. will require more playing around.
-		unsigned int limit = tvals.size();
-		for (unsigned int i = 0; i < limit; i++)
-		{
-			try
-			{
-				returnme.push_back(boost::lexical_cast<T>(tvals[i]));	
-			}
-			catch (...) {};
-		};
-	};
-	return returnme;
+private:
+  typedef std::multimap<std::string,std::string> value_list_type;
+  value_list_type values;
+public:
+  /// conf_reader iterator, points to a conf_reader::pair
+  typedef value_list_type::iterator iterator;
+  /// conf_reader::pair is a std::pair<string,string>
+  typedef std::pair<std::string,std::string> pair;
+  /** No argument constructor; actually calls read() without a filename.
+  **/
+  conf_reader(std::string name, log_queue & l);
+  /** NOP virtual destructor to allow safe inheritance.
+  **/
+  virtual ~conf_reader();
+  /** Reads and parses the command line and the provided file.
+  ** 
+  ** Returns the number of values stored. argc and argv are, of 
+  ** course the same names you used as arguments to main(). Therefore
+  ** you can easily read all command line and configuration file 
+  ** values like this:
+  ** 
+  ** int main(int argc, char* argv[])
+  **
+  ** {
+  **
+  ** 	conf_reader config;
+  **
+  ** 	config.read(argc,argv,"my_conf_file");
+  **
+  ** 	...
+  **
+  ** }
+  ** 
+  ** See read(filename,_clear) for a discription of how configuration
+  ** files are structured and parsed. This method unconditionally
+  ** clears the existing value list before starting. For command line
+  ** arguments the following rules are true.
+  ** 
+  ** 1. The executable name (argv[0]) is stored as a value with the 
+  **    name "__exec_name".
+  **   
+  ** 2. Command line arguments of the form "name=value" are parsed
+  **    stored as named values.
+  ** 
+  ** 3. All other command line arguments are stored as names with 
+  **    value = "".
+  **    
+  ** 4. A command line argument of form "configfile=filename" will
+  **    override the filename supplied as an argument to this method.
+  **    
+  ** 5. In the case of duplicate command line arguments, the last one 
+  **    specified wins.
+  **    
+  ** 6. In the case of names in the configuration file duplicating 
+  **    names from the command line, the values from the command line
+  **    dominate. If there were multiple values for a given name 
+  **    specified in the file, only the first one (the one returned by
+  **    the "[]" operator or get<>() method) is overridden.
+  **/
+  unsigned int read(int argc, char* argv[], 
+	  const std::string & _filename);
+  /// clears the name/value list.
+  void clear();
+  /// returns an iterator to the first item in the list.
+  iterator begin();
+  /// returns an iterator one past the end of the list.
+  iterator end();
+  /// True if there are no values, false otherwise.
+  bool empty();
+  /// Returns the number of values stored.
+  unsigned int size();
+  /** Returns the string value matching the supplied name.
+  ** 
+  ** NOTE: the use of get<T>(key) or get<T>(key,default) is preferred.
+  ** This method is public to allow for specialized handling in the
+  ** rare cases where it may be required.
+  ** 
+  ** If there are no matching names, "" is returned. Be aware that
+  ** "" is a valid value, so you can not use this to verify that
+  ** a given name was defined. Use exists() for that.
+  **/
+  std::string operator [] (const std::string & key);
+  /** Returns all values associated with the supplied name.
+  ** 
+  ** NOTE: the use of getall<T>(key) is preferred. This method is 
+  ** public to allow specialized handling in rare case where it
+  ** may be required.
+  ** 
+  ** If there are no values defined  the strlist will be empty.
+  **/
+  strlist all(const std::string & key);
+  /// True if the name exists, false otherwise.
+  bool exists(const std::string & key);
+  /** Use this to get all matching values.
+  ** 
+  ** Usage:
+  ** 
+  ** vector<type> mylist = conf_reader_object.getall<type>(key);
+  ** 
+  ** type can be any standard type (string, int, double, etc.) or any
+  ** type for which the ">>" stream operator is defined. All values
+  ** with matching names that can map to the requested type will be
+  ** returned. Any that do not map will not be returned.
+  **/
+  template < class T >
+    typename std::vector<T> getall(const std::string & key);
+  /** Use this to get the matching value.
+  ** 
+  ** Useage:
+  ** 
+  ** type myvar = get<type>(key);
+  ** 
+  ** type can be any standard type (string, int, double, etc.) or any
+  ** type for which the ">>" stream operator is defined. The return
+  ** value is initialized to all zeros if no matching name is found or
+  ** if the first value with that name does not map to the requested 
+  ** type. For the numeric types that results in zero being returned,
+  ** but be aware that more complex object may be in non-sensible
+  ** states if they were not found or could not map. Use exists() to 
+  ** verify the existance of a given name if needed.
+  **/
+  template < class T >
+    T get(const std::string & key);
+  /** Returns the value for the requested key, or the supplied default.
+  ** 
+  ** Works exactly like get<>() with the exception that if the key
+  ** is not found in the list, the user supplied default value is
+  ** returned instead of 0 or an empty string. If the value exists but
+  ** can not map to the requested type, 0 or an empty string is 
+  ** returned for the standard types.
+  **/
+  template <class T>
+    T get(const std::string & key, const T & def);
+private:
+  std::string filename;
+  log_recorder logger;
+protected:
+  /** Reads and parses a configuration file.
+  ** 
+  ** Returns the number of values stored.
+  ** 
+  ** The optional parameter _clear when true clears the currently
+  ** stored list of values. if false the new values read will be added
+  ** to the existing list.
+  ** 
+  ** If the file named can not be processed completely, a warning
+  ** message is published to cerr. 
+  ** 
+  ** Configuration files are defined as follows.
+  ** 
+  ** 1. Files are read line by line. No value can cross a newline
+  **    (and at least in this version) or include one. The good
+  **    news is that you are allowed up to 16k for each line.
+  **    
+  ** 2. values are specifed in name/value pairs, one per line, with
+  ** 	 the name first, followed by whitespace, followed by the 
+  ** 	 value. Values may include whitespace. All leading and trailing
+  ** 	 whitespace is removed from both the name and the value.
+  ** 	 
+  ** 3. Comments start with a \# symbol; all text following a \# is
+  **    ignored. If you need to use \# in a name or value, escape
+  **    it with a backslash instead.
+  **    
+  ** 4. Duplicate names are allowed.
+  ** 
+  ** 5. Names without values will be stored with "" as the value.
+  ** 
+  ** 6. A configuration file may include another configuration file
+  **    automatically by the use of the "*INCLUDE" reserved name.
+  **    When this name is found, it's value is used as the name of
+  **    the new file to be read.
+  **/
+  unsigned int read(const std::string & _filename = "", bool _clear=true);
 };
 
 template < class T >
-	T conf_reader::get(const std::string & key)
+  typename std::vector<T> conf_reader::getall(const std::string & key)
 {
-	conf_reader & me = *this;
-	std::string tval = me[key];
-	T returnme;
-	// initialize the return value to nulls
-	// Needed for the numeric types, but bad for strings.
-	if (typeid(T) != typeid(std::string))
-	{
-		// null out the working area (death for strings!)
-		memset(&returnme,0,sizeof(T));
-	};
-	// This does appear to work for all the standard types.
-	if (tval != "")
-	{
-		try
-		{
-			returnme = boost::lexical_cast<T>(tval);
-		}
-		catch (...) {};
-	};
-	return returnme;
+  strlist tvals = all(key);
+  std::vector<T> returnme;
+  if (typeid(T) == typeid(std::string))
+  {
+    // T is a std::string.. we can do this quickly.
+    strlist * wl = (strlist *) &returnme;
+    *wl = tvals;
+  }
+  else
+  {
+    // T is non-string.. will require more playing around.
+    unsigned int limit = tvals.size();
+    for (unsigned int i = 0; i < limit; i++)
+    {
+      try
+      {
+	returnme.push_back(boost::lexical_cast<T>(tvals[i]));	
+      }
+      catch (...) {};
+    };
+  };
+  return returnme;
 };
 
 template < class T >
-	T conf_reader::get(const std::string & key, const T & def)
+  T conf_reader::get(const std::string & key)
 {
-	if (exists(key)) { return get<T>(key); }
-	else { return def; };
+  conf_reader & me = *this;
+  std::string tval = me[key];
+  T returnme;
+  // initialize the return value to nulls
+  // Needed for the numeric types, but bad for strings.
+  if (typeid(T) != typeid(std::string))
+  {
+    // null out the working area (death for strings!)
+    memset(&returnme,0,sizeof(T));
+  };
+  // This does appear to work for all the standard types.
+  if (tval != "")
+  {
+    try
+    {
+      returnme = boost::lexical_cast<T>(tval);
+    }
+    catch (...) {};
+  };
+  return returnme;
+};
+
+template < class T >
+  T conf_reader::get(const std::string & key, const T & def)
+{
+  if (exists(key)) { return get<T>(key); }
+  else { return def; };
 };
 
 }; // namespace nrtb
 
-#endif // ricklib_confreader_h
+#endif // nrtb_confreader_h
 
