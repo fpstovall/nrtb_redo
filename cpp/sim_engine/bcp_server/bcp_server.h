7d80e313a10527d8c72cfbd270d1afa9a9ec85cc
Rick Stovall <fpstovall@gmail.com>
2014-10-12 16:40:16 -0400

    Completed bcp_listener header.
    Task @T52 : I believe this is the header for the BCP listener.
    None of the methods are implemented yet, so of course this
    compiles but does nothing.

--- cpp/sim_engine/bcp_server/bcp_server.h
+++ cpp/sim_engine/bcp_server/bcp_server.h
@@ -19,14 +19,31 @@
 #ifndef bcp_server_header
 #define bcp_server_header
 
-#include <bot_mk1.h>
 #include <base_socket.h>
 #include <confreader.h>
 #include <common_log.h>
+#include <bot_mk1.h>
+#include <sim_core.h>
 
 namespace nrtb
 {
 
+class bcp_listener
+{
+public:
+  bcp_listener(sim_core & e);
+  ~bcp_listener();
+  
+  int connections();
+  bool listening();
+  void stop();
+  
+private:
+  sim_core & engine;
+  std::thread p_thread;
+  tcp_server_socket_factory listener;
+  void processor();
+};
 
 } // namepace nrtb
 
