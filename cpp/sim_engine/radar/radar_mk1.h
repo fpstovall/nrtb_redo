fabd9059232087237e60add8572551f4ccabdcad
Rick Stovall <fpstovall@gmail.com>
2014-11-10 21:10:17 -0500

    Starting the radar_mk1 class
    Task @T60 : Empty templates for the radar_mk1 sensor class. Just a
    bunch of empty files at the moment; it compiles but does nothing.

---
+++ cpp/sim_engine/radar/radar_mk1.h
@@ -0,0 +1,30 @@
+/***********************************************
+ This file is part of the NRTB project (https://github.com/fpstovall/nrtb_alpha).
+ 
+ NRTB is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NRTB is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NRTB.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+ 
+#ifndef radar_mk1_header
+#define radar_mk1_header
+
+#include <triad.h>
+#include <base_object.h>
+
+namespace nrtb
+{
+
+} // namepace nrtb
+
+#endif // radar_mk1_header
---
+++ cpp/sim_engine/radar/radar_mk1_test.cpp
@@ -0,0 +1,66 @@
+/***********************************************
+ This file is part of the NRTB project (https://github.com/fpstovall/nrtb_alpha).
+ 
+ NRTB is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NRTB is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NRTB.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+
+#include "radar_mk1.h"
+#include <bot_mk1.h>
+#include <iostream>
+#include <string>
+
+using namespace nrtb;
+using namespace std;
+
+
+int main()
+{
+  bool failed = false;
+  cout << "========== radar_mk1 test ============="
+    << endl;
+
+    
+    cout << "=========== radar_mk1 test complete ============="
+    << endl;
+  
+  return failed;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
