cd0845468aa166be945d5e245b90427b4741e09a
Rick Stovall <fpstovall@gmail.com>
2014-11-24 19:32:38 -0500

    Roughed out templates for the rail_gun.
    Task @T86 : This is just the roughed out file structure for the
    rail gun class. The files are content free and pretty useless as
    it stands now, but it's a start.

---
+++ cpp/sim_engine/rail_gun/rail_gun.h
@@ -0,0 +1,31 @@
+/***********************************************
+ This file is part of the NRTB project (https://github.com/fpstovall/nrtb_alpha).
+ 
+ NRTB is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NRTB is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NRTB.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+ 
+#ifndef rail_gun_header
+#define rail_gun_header
+
+#include <sim_core.h>
+#include <hires_timer.h>
+
+namespace nrtb
+{
+  
+
+} // namepace nrtb
+
+#endif // rail_gun_header
---
+++ cpp/sim_engine/rail_gun/rail_gun_test.cpp
@@ -0,0 +1,92 @@
+/***********************************************
+ This file is part of the NRTB project (https://github.com/fpstovall/nrtb_alpha).
+ 
+ NRTB is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ NRTB is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with NRTB.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ **********************************************/
+
+#include "rail_gun.h"
+#include <base_object.h>
+#include <iostream>
+#include <string>
+
+using namespace nrtb;
+using namespace std;
+
+struct my_object : public base_object
+{
+  my_object()
+  {
+    location = triplet(0,0,0);
+    velocity = triplet(0,0,0);
+    bounding_sphere.center = triplet(0,0,0);
+    bounding_sphere.radius = 1;
+    mass = 1;
+  };
+  
+  
+  bool apply_collision(object_p o) 
+  {
+    return true;
+  };
+
+  base_object * clone()
+  {
+    my_object * returnme = new my_object(*this);
+    returnme->pre_attribs = get_pre_attribs_copy();
+    returnme->post_attribs = get_post_attribs_copy();
+    return returnme;
+  };
+};
+
+
+int main()
+{
+  bool failed = false;
+  cout << "========== rail_gun test ============="
+    << endl;
+ 
+    
+  cout << "=========== rail_gun test complete ============="
+    << endl;
+  
+  return failed;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
