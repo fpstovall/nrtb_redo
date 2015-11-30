d604b55325760b29b416848969ae94b50b4d572b
Rick Stovall <fpstovall@gmail.com>
2015-11-29 21:26:51 -0500

    Task #41 : First serious cut at the kamikaze bot.
    This compiles and runs, but is not picking up the target via radar.

--- cpp/BCPs/kamikaze/kamikaze.cpp
+++ cpp/BCPs/kamikaze/kamikaze.cpp
@@ -29,9 +29,126 @@ using namespace std;
 
 typedef triad<float> triplet;
 
+struct lvar
+{
+  triplet location;
+  triplet velocity;
+  triplet attitude;
+  triplet rotation;
+  void load(string data)
+  {
+    string junk;
+    stringstream s(data);
+    s >> junk >> junk // "bot lvar"
+      >> location >> velocity >> attitude >> rotation;
+  }
+};
+
+class async_com_handler
+{
+public:
+  async_com_handler(std::string addr)
+    : address(addr), active(false)
+  {
+    worker_thread = std::thread(async_com_handler::worker, std::ref(*this));
+  };
+  ~async_com_handler()
+  {
+    if (active) try {close();} catch (...) {};
+  };
+  std::string get()
+  {
+    return inbound.pop();
+  };
+  void put(std::string msg)
+  {
+    sock.put(msg+"\r");
+  };
+  void close()
+  {
+    active = false;
+    sock.close();
+    if (worker_thread.joinable()) worker_thread.join();
+  };
+private:
+  linear_queue<std::string> inbound;
+  std::string address;
+  std::atomic<bool> active;
+  tcp_socket sock;
+  std::thread worker_thread;
+  static void worker(async_com_handler & a)
+  {
+    a.active = true;
+    try
+    {
+      // Make the socket connection.
+      a.sock.connect(a.address);
+      // loop
+      while (a.active)
+      {
+        try
+        {
+          a.inbound.push(gsub(a.sock.getln("\r",0,1),"\r",""));
+        }
+        catch (tcp_socket::timeout_exception e) {};
+      };
+    }
+    catch (tcp_socket::not_open_exception & e)
+    {
+      // nop... this is normal af the end.
+    }
+    catch (base_exception & e) 
+    {
+      cerr << "Exception at " << __FILE__ << ":" << __LINE__ << endl;
+      cerr << e.what() << "\n" << e.comment() << endl;
+    }
+    catch (std::exception & e)
+    {
+      cerr << "Exception at " << __FILE__ << ":" << __LINE__ << endl;
+      cerr << e.what() << endl;
+    }
+    catch (...) 
+    {
+      cerr << "Unknown error at " << __FILE__ << ":" << __LINE__ << endl;
+    };
+    // close out the connection
+    try {a.sock.close();} catch (...) {};
+    a.active = false;
+  };
+};
+
+lvar get_my_state(async_com_handler & s)
+{
+  lvar returnme;
+  s.put("bot lvar");
+  returnme.load(s.get());
+  return returnme;
+};
+
+lvar get_target(async_com_handler & s)
+{
+  lvar returnme;
+  int tossme;
+  s.put("radar contacts");
+  std::stringstream contacts(s.get());
+  int count;
+  contacts >> count;
+  if (count > 0)
+  {
+    contacts >> tossme 
+      >> returnme.location
+      >> returnme.velocity;
+  }
+  else
+  {
+    returnme.location.x = -1.0;
+  };
+  return returnme;
+};
+
 int main(int argc, char * argv[])
 {
-  hirez_timer runtime();
+  hirez_timer runtime;
   // load the global configuration
   conf_reader config;
   config.read(argc, argv, "kamikaze.conf");
@@ -39,15 +156,59 @@ int main(int argc, char * argv[])
   // get the configuration data
   string server_addr(config.get<string>("server","127.0.0.1:64500"));
   
-  tcp_socket sim;
+  // start the comm handler.
+  async_com_handler sim(server_addr);
+  
   try
   {
     string sys, verb; 
-    sim.connect(server_addr);
+    lvar current;
+    lvar target;
+    int counter(0);
+
     // get bot ack.
-    cout << "\n" << sim.getln() << endl;
+    cout << "\n" << sim.get() << endl;
     
+    // get our current conditions
+    while (true)
+    {
+      current = get_my_state(sim);
+      target = get_target(sim);
       
+      // Status report
+      if ((counter++ % 25) == 0)
+      {
+        cout << runtime.interval_as_HMS()
+          << "\n\tvct to target: " << target.location
+          << "\n\t     location: " << current.location
+          << "\n\t      heading: " << current.attitude.z
+          << "\n\t        speed: " << current.velocity.magnatude()
+          << endl;
+      };
+      
+      if (target.location.x == -1.0)
+      {
+        // no current target to chase
+        sim.put("drive power 0");
+        sim.put("drive brake 100");
+      }
+      else
+      {
+        // go get'm!
+        sim.put("drive brake 0");
+        sim.put("drive power 100");
+        // figure out our course changes. 
+        float delta = (target.location.y -current.attitude.z);
+        if (delta < -0.3) 
+          { sim.put("drive turn -100"); }
+        else if (delta > -0.3) 
+          { sim.put("drive turn 100"); }
+        else { sim.put("drive turn 0"); };
+      };
+      const int t(1e6/50);
+      std::chrono::microseconds st(t);
+      this_thread::sleep_for(st);
+    };
     
   }
   catch (...)
