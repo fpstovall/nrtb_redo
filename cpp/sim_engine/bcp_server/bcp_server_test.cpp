c648aef4381cda607c2907031b9e93dc34d753c3
Rick Stovall <fpstovall@gmail.com>
2018-02-02 20:11:51 -0500

    Cleaned up two unit tests which used sim_core
    Unit tests passed and ready to go.

--- cpp/sim_engine/bcp_server/bcp_server_test.cpp
+++ cpp/sim_engine/bcp_server/bcp_server_test.cpp
@@ -40,7 +40,7 @@ int main()
 
   // use the listener to make some bots.
   sim_core sc1(1.0/50.0);
-  sc1.start_sim();
+  sc1.start_sim(2);
   bcp_listener listener(sc1,"*:64500",100);
   listener.start();
   std::vector<tcp_socket_p> bcps;
